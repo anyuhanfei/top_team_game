d6bf1367cf4b2f10739225f1e076c74bfe5a1028
anyuhanfei <1223050251@qq.com>
2020-10-12 17:15:59 +0800

    0.2.11 首页样式 and 中英文修复

--- app/lang/en-us.php
+++ app/lang/en-us.php
@@ -299,5 +299,6 @@ return [
     '我的助记词'=> 'My mnemonic',
     '微信二维码'=> 'WeChat qr code',
     '微信号'=> 'Wechat ID',
-    '暂未开放...'=> 'Temporarily not opened...'
+    '暂未开放...'=> 'Temporarily not opened...',
+    '价格'=> ' price'
 ];
